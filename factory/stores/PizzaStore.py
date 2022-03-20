9aef8f9e83eefd6ed41ce8e507631620d24ae21e
gsegon <gordan.segon@gmail.com>
2022-03-20 21:16:29 +0100

    #1024 Added comments

--- factory/stores/PizzaStore.py
+++ factory/stores/PizzaStore.py
@@ -2,9 +2,19 @@ from abc import ABC, abstractmethod
 from factory.pizzas.Pizza import Pizza
 
 
+# This is our abstract creator class. It defines an abstract factory
+# method that the subclasses implement to produce products
 class PizzaStore(ABC):
+    """
+    This is our abstract creator class. It defines an abstract factory
+    method that the subclasses implement to produce products.
+    """
 
     def order_pizza(self, pizza_type):
+        """
+        Code that depends on an abstract product which is produced by a subclass.
+        The creator never really knows which concrete product was produced
+        """
 
         pizza = self.create_pizza(pizza_type)
 
@@ -17,4 +27,5 @@ class PizzaStore(ABC):
 
     @abstractmethod
     def create_pizza(self, pizza_type: str) -> Pizza:
+        """ Factory method (abstract) """
         pass
